b3299f9f6ad1ba39e6bf8ceceaab4ff116b6f8ab
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-03-10 22:08:21 +0300

    Game Page: in progress.

--- src/model/generic/GenericObjectsWithStaticTools.ts
+++ src/model/generic/GenericObjectsWithStaticTools.ts
@@ -7,9 +7,14 @@ export class GenericObjectsWithStaticTools {
     static getStaticObject<StaticType extends IGenericObjectVO = IGenericObjectVO>(object: IGenericObjectWithStaticVO): StaticType {
         let result: StaticType;
 
-        if (object.staticType && object.staticId) {
+        if (object.staticType) {
+            let staticId: string = object.staticId;
+            if (!staticId) {
+                staticId = object.id;
+            }
+
             const genericByTypeModel: GenericObjectsByTypeModel = getInstance(GenericObjectsByTypeModel);
-            result = genericByTypeModel.getItem(object.staticType, object.staticId);
+            result = genericByTypeModel.getItem(object.staticType, staticId);
         }
 
         return result;
--- src/model/generic/IGenericObjectWithStaticVO.ts
+++ src/model/generic/IGenericObjectWithStaticVO.ts
@@ -1,6 +1,7 @@
 import {IGenericObjectVO} from "./IGenericObjectVO";
 
-export interface IGenericObjectWithStaticVO extends IGenericObjectVO {
+export interface IGenericObjectWithStaticVO<StaticDataType extends IGenericObjectVO = IGenericObjectVO> extends IGenericObjectVO {
     staticId?: string;
     staticType?: string;
+    staticData?: StaticDataType;
 }
