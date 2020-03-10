0e5e96e0c7dafc4515ba6c6bda5c1ce478d1cba7
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-03-10 20:12:02 +0300

    In progress.

--- src/base/data/BaseDataVO.ts
+++ src/base/data/BaseDataVO.ts
@@ -1,52 +1,18 @@
 ﻿import {Logger, ObjectTools, BaseEventDispatcher} from "fcore";
 
 import {IGenericObjectVO} from "../..";
+import {BaseDataVOEvent} from "./BaseDataVOEvent";
 
 export class BaseDataVO extends BaseEventDispatcher implements IGenericObjectVO {
 
-    public type:string = "";
-    public id:string = "";
+    public type: string = "";
+    public id: string = "";
 
-    private sourceData:any = {};
+    update(source: any): void {
+        const isChanged: boolean = ObjectTools.copyProps(this, source);
 
-    protected sourceDataPropNamesMap:any = {};
-
-    protected commitSourceData():void {
-        var propName:string;
-        var propValue:any;
-
-        var sourcePropName:any;
-        for (sourcePropName in this.sourceData) {
-            propName = sourcePropName;
-
-            // If there is a special name for the processed property,
-            // then use the special name instead of the original name
-            if (this.sourceDataPropNamesMap[sourcePropName]) {
-                propName = this.sourceDataPropNamesMap[sourcePropName];
-            }
-
-
-            if (propName) {
-                propValue = this.sourceData[sourcePropName];
-
-                if (propValue != null) {
-                    if (this.hasOwnProperty(propName) ||
-                        (this["__proto__"] && this["__proto__"].hasOwnProperty(propName))) {
-                        try {
-                            this[propName] = propValue;
-
-                        } catch (error) {
-                            Logger.error("BaseDataVO | commitSourceData __ ERROR! error: " + error);
-                        }
-                    }
-                }
-            }
+        if (isChanged) {
+            this.dispatchEvent(BaseDataVOEvent.CHANGE);
         }
-    }
-
-    public changeSourceData(changesData:any):void {
-        ObjectTools.copyProps(this.sourceData, changesData);
-
-        this.commitSourceData();
     }
 }
--- src/model/generic/IGenericObjectVO.ts
+++ src/model/generic/IGenericObjectVO.ts
@@ -1,8 +1,10 @@
+import {GenericObjectChangeActionType} from "./GenericObjectChangeActionType";
+
 export interface IGenericObjectVO {
     id?: string;
     type?: string;
 
-    action?: string;
+    action?: GenericObjectChangeActionType;
     update?:(source:any) => void;
 
     [key: string]: any;
