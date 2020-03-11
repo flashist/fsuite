f238e95c31dd7cf5aac0bd7ea3d6001f7587a47b
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-03-11 20:16:06 +0300

    In progress.

--- src/model/generic/IGenericObjectVO.ts
+++ src/model/generic/IGenericObjectVO.ts
@@ -7,5 +7,7 @@ export interface IGenericObjectVO {
     action?: GenericObjectChangeActionType;
     update?:(source:any) => void;
 
+    // This is needed because since some typescript version it's not possible
+    // to use object with fields, not described in an interface
     [key: string]: any;
 }
