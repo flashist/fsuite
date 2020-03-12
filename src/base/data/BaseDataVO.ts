1a7552b4c5c37d4c1da295e5384c15f9c80034f2
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-03-12 23:17:21 +0300

    Game Page: in progress.

--- src/base/data/BaseDataVO.ts
+++ src/base/data/BaseDataVO.ts
@@ -8,11 +8,12 @@ export class BaseDataVO extends BaseEventDispatcher implements IGenericObjectVO 
     public type: string = "";
     public id: string = "";
 
-    update(source: any): void {
+    update(source: Partial<BaseDataVO>): void {
         const isChanged: boolean = ObjectTools.copyProps(this, source);
 
         if (isChanged) {
             this.dispatchEvent(BaseDataVOEvent.CHANGE);
         }
     }
+
 }
